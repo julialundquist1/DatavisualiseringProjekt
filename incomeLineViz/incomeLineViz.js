ad5e7861574792fd52f108d6a0e21a1751b85f5e
Alba Girlinger <albagirlinger@hotmail.com>
2025-05-16 11:17:45 +0200

    show lines

--- incomeLineViz/incomeLineViz.js
+++ incomeLineViz/incomeLineViz.js
@@ -1,4 +1,4 @@
-function renderIncomeLineViz () {
+function renderIncomeLineViz() {
     const wSvg = 950;
     const hSvg = 600;
     const wViz = wSvg * 0.8;
@@ -24,15 +24,15 @@ function renderIncomeLineViz () {
         for (let i = 2015; i <= 2024; i++) {
             let djIncome = 0;
             for (let gig of managerGigs) {
-                let date = new Date (gig.date);
+                let date = new Date(gig.date);
                 let year = date.getFullYear();
                 if (year == i) {
                     djIncome += gig.djEarnings;
                 }
-                
+
             }
             let averageIncome = djIncome / numberOfDjs;
-            let incomePoint = {year: i, income: averageIncome};
+            let incomePoint = { year: i, income: averageIncome };
             incomeObject.years.push(incomePoint);
         }
 
@@ -41,14 +41,14 @@ function renderIncomeLineViz () {
 
     let colorArray = ["#00FFFF", "#FF7317", "#FF2D1F", "#BB00FF", "#480892", "#FFFB00", "#055C00", "#0333ED", "#2FFF00", "#FE00B0"];
     let allYears = incomeData[0].years.map(y => y.year);
-    
+
     let maxIncome = incomeData[0].years[0].income;
     for (let income of incomeData) {
-        for(let point of income.years) {
+        for (let point of income.years) {
             maxIncome = Math.max(maxIncome, point.income);
         }
     }
-    
+
     let incomeScale = d3.scaleLinear([0, maxIncome], [hPaddingTop + hViz, hPaddingTop]);
     let yearScale = d3.scalePoint(allYears, [wPadding, wPadding + wViz])
         .padding(0);
@@ -65,8 +65,8 @@ function renderIncomeLineViz () {
         .call(xAxis)
         .selectAll("text")
         .attr("class", "incomeVizText")
-        .attr("fill", "white"); 
-        ;
+        .attr("fill", "white");
+    ;
 
     let yAxis = d3.axisLeft(incomeScale);
     svg.append("g")
@@ -82,7 +82,7 @@ function renderIncomeLineViz () {
     const dMaker = d3.line()
         .x(d => yearScale(d.year))
         .y(d => incomeScale(d.income));
-    
+
     svg.append("g")
         .selectAll("path")
         .data(incomeData)
@@ -105,46 +105,60 @@ function renderIncomeLineViz () {
             .attr("cx", d => yearScale(d.year))
             .attr("cy", d => incomeScale(d.income))
             .attr("r", 5)
-        ;
+            ;
     });
 
     svg.append("text")
-    .attr("class", "incomeP")
-    .attr("x", wPadding - 30)             
-    .attr("y", hPaddingTop - 20)             
-    .attr("fill", "white")
-    .text("Income");
+        .attr("class", "incomeP")
+        .attr("x", wPadding - 30)
+        .attr("y", hPaddingTop - 20)
+        .attr("fill", "white")
+        .text("Income");
 
-svg.append("text")
-    .attr("class", "incomeP")
-    .attr("x", wPadding + wViz / 2)
-    .attr("y", hPaddingTop + hViz + 60)     
-    .attr("text-anchor", "middle")        
-    .attr("fill", "white")
-    .text("Years");
-    
+    svg.append("text")
+        .attr("class", "incomeP")
+        .attr("x", wPadding + wViz / 2)
+        .attr("y", hPaddingTop + hViz + 60)
+        .attr("text-anchor", "middle")
+        .attr("fill", "white")
+        .text("Years");
+
+
+    let activeLines = [];
     d3.select("body").append("div")
-    .selectAll("div")
-    .data(incomeData)
-    .enter()
-    .append("button")
-    .text(d => d.name)
-    .on("click", (event,d) => {
-        d3.selectAll(".incomeLine").attr("visibility", "hidden");
-        d3.selectAll("circle").attr("visibility", "hidden");
-    
-        // Hantera knappens 'active' klass
-        let activeButton = document.querySelector("button.active");
-        if (activeButton) activeButton.classList.remove("active");
-        event.target.classList.add("active");
-    
-        // Visa vald linje och cirklar
-        d3.select(`#line_${d.id}`).attr("visibility", "visible");
-        d3.selectAll(`#circle_${d.id}`).attr("visibility", "visible");
-      })
+        .selectAll("div")
+        .data(incomeData)
+        .enter()
+        .append("button")
+        .text(d => d.name)
+        .on("click", (event, d) => {
+            const lineId = `line_${d.id}`;
+            const circleId = `circle_${d.id}`;
 
+            const isActive = activeLines.includes(d.id);
+
+            if (isActive) {
+                activeLines = activeLines.filter(id => id !== d.id);
+                event.target.classList.remove("active");
+            } else {
+                activeLines.push(d.id);
+                event.target.classList.add("active");
+            }
+
+            if (activeLines.length === 0) {
+                d3.selectAll(".incomeLine").attr("visibility", "visible");
+                d3.selectAll("circle").attr("visibility", "visible");
+            } else {
+                d3.selectAll(".incomeLine").attr("visibility", "hidden");
+                d3.selectAll("circle").attr("visibility", "hidden");
+                activeLines.forEach(id => {
+                    d3.select(`#line_${id}`).attr("visibility", "visible");
+                    d3.selectAll(`#circle_${id}`).attr("visibility", "visible");
+                });
+            }
+        });
 }
 
 
-    
+
 
